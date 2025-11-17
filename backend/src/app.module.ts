654cfa760e1e5a112da0aea369b84ec3c4d4c5a3
Samrat <ksamrat224@gmail.com>
2025-11-17 21:37:01 +0545

    feat:auth module created with no entry points

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,9 +7,10 @@ import { PollsModule } from './polls/polls.module';
 import { VotesModule } from './votes/votes.module';
 import { FeedbacksModule } from './feedbacks/feedbacks.module';
 import { PollOptionsModule } from './poll-options/poll-options.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, PollsModule, VotesModule, FeedbacksModule, PollOptionsModule],
+  imports: [UsersModule, PollsModule, VotesModule, FeedbacksModule, PollOptionsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
